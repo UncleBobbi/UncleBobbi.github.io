0a0353f53b131d4c57e4c869eddb9303b7b09c1e
UncleBobbi <72516787+UncleBobbi@users.noreply.github.com>
2022-02-23 22:36:52 +0500

    Delete js directory

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-const iconMenu = document.querySelector('.menu__icon');
-if (iconMenu){
-    const menu = document.querySelector('.menu');
-    iconMenu.addEventListener("click", function (e){
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menu.classList.toggle('_active');
-        })
-  }
