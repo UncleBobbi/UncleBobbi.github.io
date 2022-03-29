c519ff4ade5cb3bc3acef4e28496b82849d417ff
UncleBobbi <72516787+UncleBobbi@users.noreply.github.com>
2022-03-29 19:36:36 +0500

    Add files via upload

---
+++ Tattoo3/js/js.js
@@ -0,0 +1,37 @@
+// console.log('TATTOO');
+// const number = 9
+//
+// console.log(number);
+//
+// number = 15
+//
+// console.log(number);
+
+function uploadYoutubeVideo() {
+
+    $(".js-youtube").each(function () {
+        $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + $(this).attr('id') + '/sddefault.jpg)');
+        $(this).append($('<div class="video__play" />'));
+    });
+
+    $('.video__play').on('click', function () {
+        let wrapp = $(this).closest('.js-youtube'),
+        videoId = wrapp.attr('id'),
+        iframe_url = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&autohide=1";
+
+        if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
+
+        let iframe = $('<iframe/>', {
+            'frameborder': '0',
+            'src': iframe_url,
+            'allow': "autoplay"
+        })
+
+        $(this).closest('.video__wrapper').append(iframe);
+
+    });
+}
+uploadYoutubeVideo();
+
+
+
