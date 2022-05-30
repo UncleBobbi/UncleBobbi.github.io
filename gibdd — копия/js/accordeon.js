67be8c5b385dae117855aa9736bdf7a9cb722af0
UncleBobbi <72516787+UncleBobbi@users.noreply.github.com>
2022-05-30 07:31:05 +0500

    Add files via upload

---
+++ gibdd — копия/js/accordeon.js
@@ -0,0 +1,22 @@
+ /*  $('.accordeon__item').on('click', function(event) {
+    
+        // console.log(self);
+        $(this).toggleClass('open');
+        if ($(this).hasClass('open')){
+          $(this).find('p').slideDown(500);
+        }
+        else {$(this).find('p').slideUp(500);}
+    }); */
+
+
+    function accordeon() {
+    $('.accordeon__item').on('click', (event) => {
+        let self = event.target,
+            item = $(self).closest('.accordeon__item');
+
+        $('.accordeon__item').not(item).removeClass('open');
+        // console.log(self);
+        item.toggleClass('open');
+    });
+}
+accordeon();
